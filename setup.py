d42652a8716aa3ab20bff3b1586397270dbb5749
Nicholas Del Nano <nickdelnano@gmail.com>
2018-10-19 21:26:54 -0400

    First commit, tox, setup.py, requirements scaffolding

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rpp-collector',
+    version='0.0.0',
+    description='Collect spotify listening history',
+    author="@ndelnano",
+    author_email="nickdelnano@gmail.com",
+    url='',
+    install_requires=[
+        'spotipy_ndelnano'
+    ],
+    license='LICENSE.txt',
+    packages=['spotify, db']
+)
---
+++ spotify/test.py
@@ -0,0 +1,6 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+
+user_id = 14 # stub
+credentials = SpotifyClientCredentials(user_id)
+print(' it works! ')
