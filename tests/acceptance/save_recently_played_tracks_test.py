a1fab3d30c677d5258b64e696ace836e9a7535ae
ndelnano <nickdelnano@gmail.com>
2019-01-25 17:48:15 +0000

    add functional test for call_recently_played_endpoint

---
+++ tests/acceptance/save_recently_played_tracks_test.py
@@ -0,0 +1,6 @@
+from db.db import get_all_users
+from spotify.save_recently_played_tracks import call_recently_played_endpoint
+
+def test_call_recently_played_endpoint():
+    test_user = get_all_users()[0]
+    print(call_recently_played_endpoint(test_user))
