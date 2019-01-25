4c37bd9b3fe1d8e7be24bcbab9f9b5e1ddba2845
ndelnano <nickdelnano@gmail.com>
2019-01-25 18:53:31 +0000

    Assert 50 tracks received from recently played

--- tests/acceptance/save_recently_played_tracks_test.py
+++ tests/acceptance/save_recently_played_tracks_test.py
@@ -3,4 +3,5 @@ from spotify.save_recently_played_tracks import call_recently_played_endpoint
 
 def test_call_recently_played_endpoint():
     test_user = get_all_users()[0]
-    assert call_recently_played_endpoint(test_user) != None
+    # Assert we got the number of tracks expected
+    assert len(call_recently_played_endpoint(test_user)) == 50
