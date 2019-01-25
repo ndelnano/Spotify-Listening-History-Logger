c6c6787404c824f82a3be3fb13551ecc6be0c5f7
ndelnano <nickdelnano@gmail.com>
2019-01-25 20:22:02 +0000

    timestamp format regression test

--- tests/acceptance/spotify/save_recently_played_tracks_test.py
+++ tests/acceptance/spotify/save_recently_played_tracks_test.py
@@ -27,7 +27,7 @@ def test_call_recently_played_endpoint():
     Spotify returns 'played_at' in this form. `2004-06-03T12:34:56.123Z`
 '''
 def test_timestamp_format_of_played_at(): 
-    spotify = get_spotify_client_for_username('test')
+    spotify = get_spotify_client_for_username('test_user')
     response = spotify.current_user_recently_played()
 
     # The structure of the response is confusing, copied this from
@@ -35,5 +35,7 @@ def test_timestamp_format_of_played_at():
     for k, api_response_tracks in response.items():
         if k == 'items':
             for api_response_track in api_response_tracks:
+                # Only do the assertion once
                 assert re.search(r'\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\dZ', api_response_track['played_at']) is not None
+                break
 
