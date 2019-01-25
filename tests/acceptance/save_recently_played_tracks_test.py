e0f13a2d1c002f432377f8aa9a27c5966ae36de0
ndelnano <nickdelnano@gmail.com>
2019-01-25 19:05:08 +0000

    Revert "get testing output, revert me"
    This reverts commit 6e0374310fdbbd502765bb20a5015c496f69fbab.

--- tests/acceptance/save_recently_played_tracks_test.py
+++ tests/acceptance/save_recently_played_tracks_test.py
@@ -4,5 +4,4 @@ from spotify.save_recently_played_tracks import call_recently_played_endpoint
 def test_call_recently_played_endpoint():
     test_user = get_all_users()[0]
     # Assert we got the number of tracks expected
-    print(call_recently_played_endpoint(test_user))
     assert len(call_recently_played_endpoint(test_user)) == 50
