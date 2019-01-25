674e1b8342d0ef1d0c282e57f33c6550e8b3e67c
ndelnano <nickdelnano@gmail.com>
2019-01-25 05:38:09 +0000

    use the form, from spotify import save_recently_played_tracks

--- tests/spotify/save_recently_played_tracks_test.py
+++ tests/spotify/save_recently_played_tracks_test.py
@@ -1,11 +1,9 @@
-from spotify.save_recently_played_tracks import iso_8601_timestamp_with_millis_and_timezone_to_seconds_since_epoch
-from spotify.save_recently_played_tracks import find_latest_timestamp
-from spotify.save_recently_played_tracks import call_recently_played_endpoint
+from spotify import save_recently_played_tracks
 
 def test_iso_8601_timestamp_with_millis_and_timezone_to_seconds_since_epoch():
     sample_timestamp_from_spotify_api = '2016-12-13T20:44:04.589Z'
     expected_unix_time = 1481661844
-    assert iso_8601_timestamp_with_millis_and_timezone_to_seconds_since_epoch(sample_timestamp_from_spotify_api) == expected_unix_time
+    assert save_recently_played_tracks.iso_8601_timestamp_with_millis_and_timezone_to_seconds_since_epoch(sample_timestamp_from_spotify_api) == expected_unix_time
 
 def test_find_latest_timestamp():
     latest_timestamp = 3000000000
@@ -13,4 +11,4 @@ def test_find_latest_timestamp():
     tracks.append({'played_at': 1000000000})
     tracks.append({'played_at': 3000000000})
     tracks.append({'played_at': latest_timestamp})
-    assert find_latest_timestamp(tracks) == latest_timestamp
+    assert save_recently_played_tracks.find_latest_timestamp(tracks) == latest_timestamp
