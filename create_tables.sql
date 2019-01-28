14209002ac41ae403326ea4f914a3efad3ce4126
ndelnano <nickdelnano@gmail.com>
2019-01-28 20:32:00 +0000

    Trim create_tables.sql to minimum, remove artist, genre, audio features schemas that I decided to not use

--- create_tables.sql
+++ create_tables.sql
@@ -1,10 +1,8 @@
-create database playlists;
 use playlists;
 /* 
  *  **tables must be created in this order due to foreign key constraints**
  *
  * TODO: cascading deletes, but not now bc we aren't ever expecting deletes.
- * TODO: Add indexes
  */
 CREATE TABLE users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -20,33 +18,12 @@ CREATE TABLE users (
   UNIQUE KEY(username)
 );
 
-CREATE TABLE artists (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100),
-  spotify_id VARCHAR(100),
-  spotify_uri VARCHAR(100),
-  genre_set INT,
-  UNIQUE KEY(spotify_uri)
-);
-
 CREATE TABLE tracks (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   spotify_uri VARCHAR(100),
   spotify_id VARCHAR(100),
   duration_ms INT,
-  danceability FLOAT(8,5),
-  energy FLOAT(8,5),
-  song_key FLOAT(8,5),
-  loudness FLOAT(8,5),
-  mode INT,
-  speechiness FLOAT(8,5),
-  acousticness FLOAT(8,5),
-  instrumentalness FLOAT(8,5),
-  liveness FLOAT(8,5),
-  valence FLOAT(8,5),
-  tempo FLOAT(8,5),
-  time_signature INT,
   UNIQUE KEY(spotify_uri)
 );
 
@@ -61,31 +38,3 @@ CREATE TABLE songs_played (
   FOREIGN KEY (track_id)
     REFERENCES tracks(id)
 );
-
-CREATE TABLE genres (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  genre VARCHAR(100)
-);
-
--- add primary key here perhaps
-CREATE TABLE artists_genres (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  artist_id INT,
-  genre_id INT,
-  FOREIGN KEY (artist_id)
-    REFERENCES artists(id),
-  FOREIGN KEY (genre_id)
-    REFERENCES genres(id)
-);
-
--- table for tracking many artists on a single track
--- add primary key here perhaps
-CREATE TABLE artists_for_track (
-  track_id INT,
-  artist_id INT,
-  FOREIGN KEY (track_id)
-    REFERENCES tracks(id),
-  FOREIGN KEY (artist_id)
-    REFERENCES artists(id)
-);
-
