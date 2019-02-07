d13f2d6be3bc0ece0b90679e239c7ca5c4ab0384
ndelnano <nickdelnano@gmail.com>
2019-02-07 18:45:54 +0000

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='recently-played-playlists',
-    version='0.0.0',
+    version='1.0.0',
     description='Poll Spotify listening history and log to MySQL, & HTTP API for recently-played-playlists-parser.',
     author="@ndelnano",
     author_email="nickdelnano@gmail.com",
